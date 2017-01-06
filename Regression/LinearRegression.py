62d05e980691b36bc21093088c46088a4b42dfb7
nonlining <nonlining@gmail.com>
2017-01-06 22:32:25 +0800

    more

--- Regression/LinearRegression.py
+++ Regression/LinearRegression.py
@@ -12,11 +12,11 @@ import numpy as np
 from math import log
 
 
-def get_residual_sum_of_squares(model, data, outcome):
+def get_residual_sum_of_squares(features_matrix, weights, outputs):
     # First get the predictions
-    pred_y = model.predict(data)
+    pred_y = predict(features_matrix, weights)
     # Then compute the residuals/errors
-    Residual = outcome - pred_y
+    Residual = outputs - pred_y
 
     # print Residual
     # Then square and add them up
@@ -33,30 +33,19 @@ def get_data(data_frame, features, output):
         output_array = []
     return(features_matrix, output_array)
 
-def predict_output(feature_matrix, weights):
+def predict(feature_matrix, weights):
     return np.dot(feature_matrix, weights)
 
 def regression_gradient_descent(feature_matrix, output, initial_weights, step_size, tolerance):
     converged = False
-    weights = np.array(initial_weights) # make sure it's a numpy array
+    weights = np.array(initial_weights)
 
     while not converged:
-        # compute the predictions based on feature_matrix and weights using your predict_output() function
-        predictions = predict_output(feature_matrix, weights)
-        # compute the errors as predictions - output
+        predictions = predict(feature_matrix, weights)
         errors =  output - predictions
-        gradient_sum_squares = 0 # initialize the gradient sum of squares
-        # while we haven't reached the tolerance yet, update each feature's weight
-        for i in range(len(weights)): # loop over each weight
-            # Recall that feature_matrix[:, i] is the feature column associated with weights[i]
-            # compute the derivative for weight[i]:
-            partial =  -2*np.dot(errors,feature_matrix[:,i])
-            # add the squared value of the derivative to the gradient sum of squares (for assessing convergence)
-            gradient_sum_squares += partial*partial
-            # subtract the step size times the derivative from the current weight
-            weights[i] = weights[i] - step_size*partial
-        # compute the square-root of the gradient sum of squares to get the gradient matnigude:
-        gradient_magnitude = sqrt(gradient_sum_squares)
+        partial =  -2*np.dot(feature_matrix.T, errors)
+        weights = weights - step_size*partial
+        gradient_magnitude = np.sqrt(np.sum(partial*partial))
         if gradient_magnitude < tolerance:
             converged = True
 
--- Regression/test_LinearRegression.py
+++ Regression/test_LinearRegression.py
@@ -31,6 +31,27 @@ def main():
     feature_bedrooms = np.array(train_data['bedrooms'])
     outputs = np.array(train_data['price'])
 
+    Model1_features = ['sqft_living']
+    Model1_output = ['price']
+    (feature_matrix1, output_vector) = SR.get_data(train_data, Model1_features, Model1_output)
+    (feature_matrix1_t, output_vector_t) = SR.get_data(test_data, Model1_features, Model1_output)
+
+    step_size1 = 7.0e-12
+    tolerance1 = 2.5e7
+    init_weights1 = np.array([-47000.0, 1.0]).reshape((2, 1))
+
+    Model1_weights = SR.regression_gradient_descent(feature_matrix1,
+                                                     output_vector,
+                                                     init_weights1,
+                                                     step_size1,
+                                                     tolerance1)
+
+
+    test1_predictions = SR.predict(feature_matrix1_t, Model1_weights)
+    print test1_predictions[0]
+    RSS1 = SR.get_residual_sum_of_squares(feature_matrix1_t, Model1_weights, output_vector_t)
+    print RSS1
+
 
 
 if __name__ == '__main__':
