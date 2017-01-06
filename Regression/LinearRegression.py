3a49129277fd60a45251ede92027cfa3082266bc
jwang32 <jwang32@lenovo.com>
2017-01-06 14:52:01 +0800

    Complete some functions

--- Regression/LinearRegression.py
+++ Regression/LinearRegression.py
@@ -23,11 +23,8 @@ def get_residual_sum_of_squares(model, data, outcome):
     RSS = (Residual*Residual).sum()
     return(RSS)
 
-def feature_derivative(errors, feature):
-    # Assume that errors and feature are both numpy arrays of the same length (number of data points)
-    # compute twice the dot product of these vectors as 'derivative' and return the value
-    derivative = 2*np.dot(errors, feature)
-    return(derivative)
+def predict_output(feature_matrix, weights):
+    return np.dot(feature_matrix, weights)
 
 def regression_gradient_descent(feature_matrix, output, initial_weights, step_size, tolerance):
     converged = False 
@@ -37,13 +34,13 @@ def regression_gradient_descent(feature_matrix, output, initial_weights, step_si
         # compute the predictions based on feature_matrix and weights using your predict_output() function
         predictions = predict_output(feature_matrix, weights)
         # compute the errors as predictions - output
-        errors = predictions - output
+        errors =  output - predictions
         gradient_sum_squares = 0 # initialize the gradient sum of squares
         # while we haven't reached the tolerance yet, update each feature's weight
         for i in range(len(weights)): # loop over each weight
             # Recall that feature_matrix[:, i] is the feature column associated with weights[i]
             # compute the derivative for weight[i]:
-            partial =  feature_derivative(errors,feature_matrix[:,i])
+            partial =  -2*np.dot(errors,feature_matrix[:,i])
             # add the squared value of the derivative to the gradient sum of squares (for assessing convergence)
             gradient_sum_squares += partial*partial
             # subtract the step size times the derivative from the current weight
@@ -54,8 +51,4 @@ def regression_gradient_descent(feature_matrix, output, initial_weights, step_si
             converged = True
 
     return(weights)
-	
-
-def Linear_Regression(train_data, target, features):
-    pass
 
