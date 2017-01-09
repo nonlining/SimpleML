5e18930077067d29aa6dd6f7072c2dbba10eb90d
nonlining <nonlining@gmail.com>
2017-01-09 22:29:57 +0800

    PolyRegression first commit

---
+++ Regression/PolynomialRegression.py
@@ -0,0 +1,29 @@
+#-------------------------------------------------------------------------------
+# Name:        polynomial regression
+# Purpose:
+#
+# Author:      Nonlining
+#
+# Created:     09/01/2017
+# Copyright:   (c) Nonlining 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+import numpy as np
+
+def polynomial_sframe(feature, degree):
+    # assume that degree >= 1
+    # initialize the SFrame:
+    poly_sframe = graphlab.SFrame()
+    # and set poly_sframe['power_1'] equal to the passed feature
+    poly_sframe['power_1'] = feature
+    # first check if degree > 1
+    if degree > 1:
+        # then loop over the remaining degrees:
+        # range usually starts at 0 and stops at the endpoint-1. We want it to start at 2 and stop at degree
+        for power in range(2, degree+1):
+            # first we'll give the column a name:
+            name = 'power_' + str(power)
+            # then assign poly_sframe[name] to the appropriate power of feature
+            poly_sframe[name] = feature.apply(lambda x: x**power)
+
+    return poly_sframe
