d94e766242f2525c37d590bfc8512190abf9156f
nonlining <nonlining@gmail.com>
2017-01-09 23:50:19 +0800

    second commit

--- Regression/PolynomialRegression.py
+++ Regression/PolynomialRegression.py
@@ -9,21 +9,18 @@
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
 import numpy as np
+import panda as pd
 
-def polynomial_sframe(feature, degree):
-    # assume that degree >= 1
-    # initialize the SFrame:
-    poly_sframe = graphlab.SFrame()
-    # and set poly_sframe['power_1'] equal to the passed feature
-    poly_sframe['power_1'] = feature
-    # first check if degree > 1
+def polynomial_dataframe(feature, degree):
+
+    polynomial_dataframe = pd.dataframe()
+
+    poly_dataframe['power_1'] = feature
+
     if degree > 1:
-        # then loop over the remaining degrees:
-        # range usually starts at 0 and stops at the endpoint-1. We want it to start at 2 and stop at degree
         for power in range(2, degree+1):
             # first we'll give the column a name:
             name = 'power_' + str(power)
-            # then assign poly_sframe[name] to the appropriate power of feature
-            poly_sframe[name] = feature.apply(lambda x: x**power)
+            poly_dataframe[name] = feature.apply(lambda x: x**power)
 
-    return poly_sframe
+    return poly_dataframe
