5a0f6058ae5f77a4b25745b3bf15eca5f973dce5
nonlining <nonlining@gmail.com>
2017-01-17 22:53:21 +0800

    Ridge Regression

---
+++ Regression/RidgeRegression.py
@@ -0,0 +1,21 @@
+#-------------------------------------------------------------------------------
+# Name:        Ridge Regression
+# Purpose:
+#
+# Author:      Nonlining
+#
+# Created:     17/01/2017
+# Copyright:   (c) Nonlining 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+import numpy as np
+
+
+def predict(features, weights):
+    return np.dot(features ,weights)
+
+
+def Ridge_Regression_gradient_descent():
+    pass
+
