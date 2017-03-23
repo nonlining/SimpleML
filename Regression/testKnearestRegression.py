328e88c5d71d22d0e81526960eb2c7e3cd141870
nonlining <nonlining@gmail.com>
2017-03-24 00:26:06 +0800

    test case file

---
+++ Regression/testKnearestRegression.py
@@ -0,0 +1,16 @@
+#-------------------------------------------------------------------------------
+# Name:        test Knearest Regression
+# Purpose:
+#
+# Author:      Nonlining
+#
+# Created:     23/03/2017
+# Copyright:   (c) Nonlining 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
