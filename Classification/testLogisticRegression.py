b887cab44e216e8fc2af1d6df1adf85605f4046f
nonlining <nonlining@gmail.com>
2017-05-19 23:05:37 +0800

    add more

--- Classification/testLogisticRegression.py
+++ Classification/testLogisticRegression.py
@@ -18,14 +18,24 @@ def main():
     print products
     print products[269]
     review_without_punctuation = products['review'].apply(LR.remove_punctuation)
-    products['word_count'] = review_without_punctuation.apply(countWords)
+    products['word_count'] = review_without_punctuation.apply(LR.countWords)
 
     print products[269]['word_count']
 
-
-
     products = products[products['rating'] != 3]
     print len(products)
+
+    products['sentiment'] = products['rating'].apply(lambda rating : +1 if rating > 3 else -1)
+
+    print products
+
+    train_data, test_data = products.random_split(.8, seed=1)
+
+    print len(train_data)
+    print len(test_data)
+
+
+
 
 if __name__ == '__main__':
     main()
