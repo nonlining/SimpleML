dd899bb3e7aaa8321cb6eeb031aeacc525106051
nonlining <nonlining@gmail.com>
2018-01-07 21:52:46 +0800

    update

--- Classification/testLogisticRegression.py
+++ Classification/testLogisticRegression.py
@@ -14,9 +14,23 @@ import numpy as np
 
 def main():
     np.random.seed(seed=1)
+
     products = pd.read_csv('../Datafiles/amazon_baby.csv')
+
+    review_without_punctuation = products['review'].apply(LR.remove_punctuation)
+    products['word_count'] = review_without_punctuation.apply(LR.countWords)
+    products['clear_words'] = review_without_punctuation
+
     # print 5 items in the list
-    print products.head()
+    print review_without_punctuation.head()
+
+    # remove all rating 3 , they are considered as neutral
+    products = products[products['rating'] != 3]
+    products['sentiment'] = products['rating'].apply(lambda rating: +1 if rating > 3 else -1)
+
+    mask = np.random.rand(len(products)) < 0.8
+
+
 
 if __name__ == '__main__':
     main()
