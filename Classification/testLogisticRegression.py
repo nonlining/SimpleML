1c1193f571259dc2055fa46354f72ef7739507cb
nonlining <nonlining@gmail.com>
2018-01-12 00:05:15 +0800

    update

--- Classification/testLogisticRegression.py
+++ Classification/testLogisticRegression.py
@@ -12,6 +12,11 @@ import LogisticRegression as LR
 import pandas as pd
 import numpy as np
 
+# import sklearn for sentiment
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn import datasets, linear_model
+from sklearn.linear_model import LogisticRegression
+
 def main():
     np.random.seed(seed=1)
 
@@ -29,6 +34,14 @@ def main():
     products['sentiment'] = products['rating'].apply(lambda rating: +1 if rating > 3 else -1)
 
     mask = np.random.rand(len(products)) < 0.8
+
+    train = products[mask]
+    test = products[~mask]
+    print "Training data size",len(train), "Testing data size",len(test)
+
+    reviews = np.array(train['clear_words'])
+    labels = np.array(train['sentiment'])
+
 
 
 
